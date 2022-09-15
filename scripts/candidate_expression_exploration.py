9f4f9d2cf33be70edcc87d96b45cb67edde6ce37
Jack Tierney <jack.tierney@student.ucc.ie>
2022-09-15 12:44:38 +0100

    plot development for distribution of expression changes

--- scripts/candidate_expression_exploration.py
+++ scripts/candidate_expression_exploration.py
@@ -1,15 +1,29 @@
 import pandas as pd 
 import argparse
+import matplotlib.pyplot as plt
 
 
 def plot_distribution(df):
     '''
     plot the distribution of expression for the given df
     '''
+    # df.plot.scatter(x="orf_count", y="cds_count", c="ratio", colormap='viridis')
+    df = df.query('file.str.startswith("Park16")',  engine="python")
+    print(df)
+    orf_count = list(df.orf_count) 
+    cds_count = list(df.cds_count)
+    annotations = list(df.file)
 
-    df.plot.hist()
 
+    plt.figure(figsize=(8,6))
+    plt.scatter(orf_count,cds_count)
+    plt.xlabel("ORF Count")
+    plt.ylabel("CDS Count")
+    plt.title(f"{list(df.transcript)[0]}",fontsize=15)
+    for i, label in enumerate(annotations):
+        plt.annotate(label, (orf_count[i], cds_count[i]))
 
+    plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
