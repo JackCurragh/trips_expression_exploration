86a2691d3fbd544685ad468b4b9f15415e25e45d
Jack Tierney <jack.tierney@student.ucc.ie>
2022-09-29 12:19:10 +0100

    added new arguments for plotting

--- scripts/candidate_expression_exploration.py
+++ scripts/candidate_expression_exploration.py
@@ -3,12 +3,12 @@ import argparse
 import matplotlib.pyplot as plt
 
 
-def plot_distribution(df):
+def plot_distribution(df, study_name):
     '''
     plot the distribution of expression for the given df
     '''
     # df.plot.scatter(x="orf_count", y="cds_count", c="ratio", colormap='viridis')
-    df = df.query('file.str.startswith("Park16")',  engine="python")
+    df = df.query(f'file.str.startswith("{study_name}")',  engine="python")
     print(df)
     orf_count = list(df.orf_count) 
     cds_count = list(df.cds_count)
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-f", help="path to transcript probabilites file")
-    
+    parser.add_argument("-s", help="Study first author name")
     args = parser.parse_args()
     df = pd.read_csv(args.f)
-    plot_distribution(df)
+    plot_distribution(df, args.s)
